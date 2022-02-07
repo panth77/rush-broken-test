e45ff47518b7e47a38a2b07b69167ba99517b1ba
Adam Roberts <aroberts@iheartmedia.com>
2022-02-07 12:23:07 -0500

    Making apollo spew.

--- applications/app1/src/app/app.component.ts
+++ applications/app1/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Apollo, gql } from 'apollo-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'app1';
+
+  a = Apollo;
+  g = gql;
 }
